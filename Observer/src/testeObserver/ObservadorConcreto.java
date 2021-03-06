5d7c50e2f202d6bc1d7851f2c8cf291268fa4359
WelberthMagalhaes <welberthheider@gmail.com>
2021-03-06 18:02:03 -0300

    modificacao

--- Observer/src/testeObserver/ObservadorConcreto.java
+++ Observer/src/testeObserver/ObservadorConcreto.java
@@ -3,6 +3,9 @@ package testeObserver;
 
 public class ObservadorConcreto implements Observer {
 
+
+	private String preco
+
 	@Override
 	public void atualiza(ObservadoConcreto o) {
 				
--- Observer/src/testeObserver/main.java
+++ Observer/src/testeObserver/main.java
@@ -4,17 +4,17 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		ObservadoConcreto Refinaria1 = new ObservadoConcreto("Petroleo");
+		ObservadoConcreto refinaria1 = new ObservadoConcreto("Petroleo");
 		
 		ObservadoConcreto refinaria2 = new ObservadoConcreto("GNV");
 		
-		ObservadorConcreto posto1 = new ObservadorConcreto(Refinaria1);
+		ObservadorConcreto posto1 = new ObservadorConcreto(refinaria1);
 		ObservadorConcreto posto2 = new ObservadorConcreto(refinaria2);
-		ObservadorConcreto posto3 = new ObservadorConcreto(Refinaria1);
+		ObservadorConcreto posto3 = new ObservadorConcreto(refinaria1);
 		ObservadorConcreto posto4 = new ObservadorConcreto(refinaria2);
 
 		
-		Refinaria1.setEstado("R$ 2,50");
+		refinaria1.setEstado("R$ 2,50");
 		
 		refinaria2.setEstado("R$ 1,65");
 		
